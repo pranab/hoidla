5fad639e5170c9d1c1cf521d1cef9e4bc2409e24
Pranab Ghosh <pkghosh99@gmail.com>
2014-10-27 08:37:23 -0700

    hyper log log

---
+++ src/main/java/org/hoidla/stream/HyperLogLog.java
@@ -0,0 +1,105 @@
+/*
+ * hoidla: various algorithms for Big Data solutions
+ * Author: Pranab Ghosh
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+
+package org.hoidla.stream;
+
+import org.hoidla.stream.UniqueItems.UniqueItemsCounter;
+import org.hoidla.util.Hashing;
+
+/**
+ * Hyper Log Log algorithm for cardinality estimation by Flajolet, Fusy, Gandouet and 
+ * @author pranab
+ *
+ */
+public class HyperLogLog implements UniqueItemsCounter {
+	private int numBucketBits;
+	private int bucketCount;
+	private Hashing.MultiHashFamily hashFamily;
+	private int[] buckets;
+	private double biasCorrection;
+	
+	/**
+	 * @param numBucketBits
+	 */
+	public HyperLogLog(int numBucketBits) {
+		super();
+		this.numBucketBits = numBucketBits;
+		bucketCount = 1 << numBucketBits - 1;
+		buckets = new int[bucketCount];
+		intializeBuckets();
+		calculateBiasCorrection();
+	}
+
+	/**
+	 * 
+	 */
+	private void intializeBuckets() {
+		for (int i = 0; i < bucketCount; ++i) {
+			buckets[i] = 0;
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	private void calculateBiasCorrection() {
+		double c = Math.log(2);
+		biasCorrection = 1.0 / (2.0 * c  * (1.0 + (3.0 * c -1) / bucketCount));
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.hoidla.stream.UniqueItems.UniqueItemsCounter#getUnqueCount()
+	 */
+	@Override
+	public long getUnqueCount() {
+		double sum = 0;
+		for (int i = 0; i < bucketCount; ++i) {
+			sum += 1.0 / (1 << buckets[i]) ;
+		}
+		return Math.round(biasCorrection / sum);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hoidla.stream.UniqueItems.UniqueItemsCounter#add(java.lang.Object)
+	 */
+	@Override
+	public void add(Object value) {
+		int hash = value.hashCode();
+		if (hash < 0){
+			hash = - hash;
+		}
+		
+		//bucket based on numBucketBits MS bits
+		int bucketIndex = hash >>> (Integer.SIZE  - numBucketBits);
+		
+		//hash for the bucket using remaining
+		int bucketHash = hash << numBucketBits | bucketCount;
+		int leadZeros = Integer.numberOfLeadingZeros(bucketHash);
+		
+		//update
+		if (leadZeros > buckets[bucketIndex]) {
+			buckets[bucketIndex] = leadZeros;
+		}
+	}
+
+	@Override
+	public void clear() {
+		intializeBuckets();
+	}
+
+}
