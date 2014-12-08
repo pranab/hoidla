0c11de9c88912ac9de4470d1a37ce82cb1198e93
Pranab Ghosh <pkghosh99@gmail.com>
2014-12-08 05:32:19 -0800

    cumulative difference sum to detect deviation from target

--- src/main/java/org/hoidla/window/WindowUtils.java
+++ src/main/java/org/hoidla/window/WindowUtils.java
@@ -240,7 +240,22 @@ public  class WindowUtils {
 	 * @param outlierThresholdFactor
 	 * @return indexes outlying data points
 	 */
-	public static List<Integer> removeOutliers(double[] data, double[] pattern, int outlierThresholdFactor) {
+	public static List<Integer> removeOutliers(double[] data, int outlierThresholdFactor) {
+		return removeOutliers(data, outlierThresholdFactor, null);
+	}
+	
+	/**
+	 * Identifies outliers
+	 * @param data
+	 * @param outlierThresholdFactor
+	 * @param pattern
+	 * @return indexes outlying data points
+	 */
+	public static List<Integer> removeOutliers(double[] data, int outlierThresholdFactor, double[] pattern) {
+		if (null != pattern && data.length != pattern.length) {
+			throw new IllegalArgumentException("data and pattern need to be of same size");
+		}
+
 		//stats
 		SummaryStatistics stats = new SummaryStatistics();
 		for (double value : data) {
@@ -253,6 +268,7 @@ public  class WindowUtils {
 		double upThreshold = mean + outlierThresholdFactor * stdDev;
 		double loThreshold = mean - outlierThresholdFactor * stdDev;
 		
+		//detect outliers
 		List<Integer> outliers = new ArrayList<Integer>();
 		int i = 0;
 		for (double value : data) {
@@ -305,6 +321,24 @@ public  class WindowUtils {
 		}		
 		dist = Math.sqrt(dist) / data.length;
 		return dist;
+	}
+	
+	/**
+	 * Detects change from target. Slope upward : average above target.Downward average below target
+	 * @param data
+	 * @param target
+	 * @return
+	 */
+	public static double[] cumultiveDiffSum(double[] data, double target) {
+		double[] cuDiffSum = new double[data.length];
+		
+		//cumulative sum of difference from target. Slope upward : average above target
+		double sum = 0;
+		for (int i = 0; i < data.length; ++i) {
+			sum += data[i] - target;
+			cuDiffSum[i] = sum;
+		}
+		return cuDiffSum;
 	}
 	
 	/**
