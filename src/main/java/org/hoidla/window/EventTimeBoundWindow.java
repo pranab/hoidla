782689b0760c533d2f4bc71b1625e74eea1b8f35
Pranab Ghosh <pkghosh99@gmail.com>
2015-12-26 00:25:20 -0800

    out of order event processing

--- src/main/java/org/hoidla/window/EventTimeBoundWindow.java
+++ src/main/java/org/hoidla/window/EventTimeBoundWindow.java
@@ -22,8 +22,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.hoidla.util.EventTimeStamped;
+import org.hoidla.util.TimeStamped;
 
 /**
  * @author pranab
@@ -36,12 +38,21 @@ public class EventTimeBoundWindow extends DataWindow<EventTimeStamped>  implemen
 	private long waterMark;
 	private int waterMarkLag;
 	private List<EventTimeStamped> eventTimeDataWindow = new ArrayList<EventTimeStamped>();
+	private long timeSpan;
+	private long timeStep = 0;
+	private long processingTimeStep = -1;
+	private long lastProcessedTime = -1;
+	
 	private static final long serialVersionUID = 8752389534053402896L;
 
-	public EventTimeBoundWindow(int maxStatWindowSize, double lagStdDevMult, int waterMarkLag) {
+	public EventTimeBoundWindow(long timeSpan, long timeStep, long processingTimeStep, int maxStatWindowSize, 
+			double lagStdDevMult, int waterMarkLag) {
 		createStatsWindow(maxStatWindowSize);
 		this.lagStdDevMult = lagStdDevMult;
 		this.waterMarkLag = waterMarkLag;
+		this.timeSpan = timeSpan;
+		this.timeStep = timeStep;
+		this.processingTimeStep = processingTimeStep;
 	}
 	
 	public void createStatsWindow(int maxStatWindowSize) {
@@ -82,18 +93,45 @@ public class EventTimeBoundWindow extends DataWindow<EventTimeStamped>  implemen
 			waterMark =  latest -  waterMarkLag;
 		}
 		
-		//only events above water mark
-		dataWindow.clear();
-		for (EventTimeStamped event : eventTimeDataWindow) {
-			if (event.getEventTimeStamp() < waterMark) {
-				dataWindow.add(event);
+		long timespandEnd = eventTimeDataWindow.get(0).getEventTimeStamp() + timeSpan;
+		if ( timespandEnd < waterMark) {
+			//only events above water mark and enough data for processing
+			dataWindow.clear();
+			for (EventTimeStamped event : eventTimeDataWindow) {
+				if (event.getEventTimeStamp() < timespandEnd) {
+					dataWindow.add(event);
+				}
 			}
+			
+			expire();
 		}
 	}
 	
 	@Override
 	public void expire() {
-		// TODO Auto-generated method stub
+		//process data
+		boolean processed = false;
+		long latest = dataWindow.get(dataWindow.size() - 1).getEventTimeStamp();
+		if (processingTimeStep > 0 && lastProcessedTime > 0) {
+			if (latest - lastProcessedTime > processingTimeStep) {
+				processFullWindow();
+				lastProcessedTime = latest;
+				processed = true;
+			}
+		} else {
+			processFullWindow();
+			lastProcessedTime = latest;
+			processed = true;
+		}
+		
+		//expire data 
+		long earliestRetained = latest - timeSpan + timeStep;
+		ListIterator<EventTimeStamped> iter =  dataWindow.listIterator();
+		while (iter.hasNext()) {
+			if (iter.next().getEventTimeStamp() < earliestRetained) {
+				iter.remove();
+			}
+		}
 		
 	}
 
