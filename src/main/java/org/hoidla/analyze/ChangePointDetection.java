d04be1d089e5950d63f5f3cba3fbfc79ed14cc02
Pranab Ghosh <pkghosh99@gmail.com>
2019-10-27 17:12:32 -0700

    change point detection with binary search

--- src/main/java/org/hoidla/analyze/ChangePointDetection.java
+++ src/main/java/org/hoidla/analyze/ChangePointDetection.java
@@ -17,13 +17,16 @@
 
 package org.hoidla.analyze;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author pranab
  *
  */
-public class ChangePointDetection {
+public class ChangePointDetection implements Serializable {
 	private double[] data;
 	
 	public ChangePointDetection(double[] data) {
@@ -42,28 +45,85 @@ public class ChangePointDetection {
 		int end = 2 * winHalfSize;
 		
 		for (int i = 0; end < data.length; ++beg, ++center, ++end, ++i) {
-			double[] leftCdf = countSegment(beg, center);
+			double[] leftCdf = countSegment(data,beg, center);
 			double leftCost = findCost(leftCdf, beg, center);
-			double[] rightCdf = countSegment(center, end);
+			double[] rightCdf = countSegment(data,center, end);
 			double rightCost = findCost(rightCdf, center, end);
-			double[] allCdf = countSegment(beg, end);
+			double[] allCdf = countSegment(data,beg, end);
 			double allCost = findCost(allCdf, beg, end);
 			double disc = allCost - (leftCost + rightCost);
 			changePoints[i] = new ChangePoint(center, disc);
 		}
 		
 		//sort descending and return top
+		return getTopChangePoints(changePoints, numChangePoints);
+	}
+	
+	/**
+	 * @param numChangePoints
+	 * @return
+	 */
+	public ChangePoint[] detectByBinarySearch(int numChangePoints, int minSegmenLength) {
+		List<ChangePoint> changePointsList = new ArrayList<ChangePoint>();
+		detectByBinarySearchHelper(data,  numChangePoints,  minSegmenLength, changePointsList);
+		
+		ChangePoint[] changePoints = new ChangePoint[changePointsList.size()];
+		changePoints = changePointsList.toArray(changePoints);
+		
+		//sort descending and return top
+		return getTopChangePoints(changePoints, numChangePoints);
+	}
+	
+	/**
+	 * @param data
+	 * @param numChangePoints
+	 * @param minSegmenLength
+	 * @param changePoints
+	 */
+	private void detectByBinarySearchHelper(double[] data, int numChangePoints, int minSegmenLength, 
+			List<ChangePoint> changePoints) {
+		if (data.length > 2 * minSegmenLength) {
+			int cpIndex = 0;
+			double maxDisc = 0;
+			for (int sp = minSegmenLength; sp < data.length - minSegmenLength; ++sp) {
+				double[] leftCdf = countSegment(data, 0, sp);
+				double leftCost = findCost(leftCdf, 0, sp);
+				double[] rightCdf = countSegment(data,sp, data.length);
+				double rightCost = findCost(rightCdf, sp, data.length);
+				double[] allCdf = countSegment(data, 0, data.length);
+				double allCost = findCost(allCdf, 0, data.length);
+				double disc = allCost - (leftCost + rightCost);
+				if (disc > maxDisc) {
+					maxDisc = disc;
+					cpIndex = sp;
+				}
+			}
+			
+			//add change point
+			changePoints.add(new ChangePoint(cpIndex, maxDisc));
+			
+			//split and recurse
+			double[] leftData = Arrays.copyOfRange(data, 0, cpIndex);
+			double[] rightData = Arrays.copyOfRange(data, cpIndex, data.length);
+			detectByBinarySearchHelper(leftData, numChangePoints,  minSegmenLength,changePoints);
+			detectByBinarySearchHelper(rightData, numChangePoints,  minSegmenLength, changePoints);
+		}
+	}
+	
+	/**
+	 * @param changePoints
+	 * @param numChangePoints
+	 * @return
+	 */
+	private ChangePoint[] getTopChangePoints(ChangePoint[] changePoints, int numChangePoints) {
+		int size = changePoints.length;
 		Arrays.sort(changePoints);
 		ChangePoint[] topChangePonts = null;
 		if (size <= numChangePoints) {
 			topChangePonts = changePoints;
 		} else {
-			topChangePonts = new ChangePoint[numChangePoints];
-			for (int i = 0; i < numChangePoints; ++i) {
-				topChangePonts[i] = changePoints[i];
-			}
+			topChangePonts = Arrays.copyOfRange(changePoints, 0, numChangePoints);
 		}
-		
 		return topChangePonts;
 	}
 	
@@ -72,7 +132,7 @@ public class ChangePointDetection {
 	 * @param end
 	 * @return
 	 */
-	private double[] countSegment(int beg, int end) {
+	private double[] countSegment(double[] data, int beg, int end) {
 		double[] counts = new double[data.length];
 		for (int i = 0; i < data.length; ++i) {
 			double count = 0;
